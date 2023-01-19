862e84afd537780089e7e4ff4bac6bd1532908b1
Jucepos <jpostigo89@gmail.com>
2023-01-19 15:19:49 -0300

    cambios

--- pipeline.py
+++ pipeline.py
@@ -2,17 +2,26 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 from sqlalchemy import create_engine
+import time
+
+# Obtiene el tiempo actual
+start_time = time.time()
+
 
 df11=pd.read_csv("Datasets/product_category_name_translation.csv")
 
 df11=df11.append({"product_category_name" : "pc_gamer" , "product_category_name_english" : "pc_gamer"} , ignore_index=True)
-df11=df11.append({"product_category_name" : "portateis_cozinha_e_preparadores_de_alimentos" , "product_category_name_english" : "kitchen and food preparation racks"} , ignore_index=True)
+df11=df11.append({"product_category_name" : "portateis_cozinha_e_preparadores_de_alimentos" , "product_category_name_english" : "kitchen_and_food_preparation_racks"} , ignore_index=True)
+
+df11["product_category_name"]=df11["product_category_name"].str.replace("_"," ")
+df11["product_category_name_english"]=df11["product_category_name_english"].str.replace("_"," ")
 
 engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-cf3enqun6mplnpe950v0-a.oregon-postgres.render.com:5432/olist')
 
 df11.to_sql('product_category_name_translation', engine, if_exists='append', index=False)
 
-engine.dispose()
+print('df11 cargada')
+
 
 geo=pd.read_csv("Datasets_auxiliares/pipeline_geo.csv", dtype= {'zip_code_prefix': str})
 
@@ -20,6 +29,7 @@ engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-
 
 geo.to_sql('geolocation', engine, if_exists='append', index=False)
 
+print('geo cargada')
 
 
 df4=pd.read_csv("Datasets/olist_marketing_qualified_leads_dataset.csv")
@@ -34,11 +44,16 @@ engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-
 
 df4.to_sql('marketing_qualified_leads', engine, if_exists='append', index=False)
 
+print('df4 cargada')
 
 
+df1=pd.read_csv("Datasets/olist_closed_deals_dataset.csv")
 df10=pd.read_csv("Datasets/olist_sellers_dataset.csv",dtype={"seller_zip_code_prefix": str})
 
 df10.drop(columns=["seller_city","seller_state"],axis=1,inplace=True)
+
+mergeauxiliar=pd.merge(left=df10,right=df1,how="outer",on="seller_id")
+df10=mergeauxiliar.iloc[:,0:2]
 
 df10['seller_zip_code_prefix'] = df10['seller_zip_code_prefix'].astype('string')
 
@@ -46,6 +61,7 @@ engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-
 
 df10.to_sql('olist_sellers', engine, if_exists='append', index=False)
 
+print('df10 cargada')
 
 
 df1=pd.read_csv("Datasets/olist_closed_deals_dataset.csv")
@@ -60,6 +76,8 @@ engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-
 
 df1.to_sql('closed_deals', engine, if_exists='append', index=False)
 
+print('df1 cargada')
+
 
 df2=pd.read_csv("Datasets/olist_customers_dataset.csv",dtype={"customer_zip_code_prefix": str})
 
@@ -69,6 +87,7 @@ engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-
 
 df2.to_sql('customers', engine, if_exists='append', index=False)
 
+print('df2 cargada')
 
 df8=pd.read_csv("Datasets/olist_orders_dataset.csv")
 
@@ -112,6 +131,8 @@ engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-
 
 df8.to_sql('orders', engine, if_exists='append', index=False)
 
+print('df8 cargada')
+
 
 df6=pd.read_csv("Datasets/olist_order_payments_dataset.csv")
 
@@ -120,6 +141,8 @@ df6.drop(columns=["payment_sequential"],axis= 1, inplace=True)
 engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-cf3enqun6mplnpe950v0-a.oregon-postgres.render.com:5432/olist')
 
 df6.to_sql('order_payments', engine, if_exists='append', index=False)
+
+print('df6 cargada')
 
 
 df7=pd.read_csv("Datasets/olist_order_reviews_dataset.csv")
@@ -134,6 +157,7 @@ engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-
 
 df7.to_sql('order_reviews', engine, if_exists='append', index=False)
 
+print('df7 cargada')
 
 
 df9=pd.read_csv("Datasets/olist_products_dataset.csv")
@@ -141,10 +165,14 @@ df9=pd.read_csv("Datasets/olist_products_dataset.csv")
 df9.drop(columns=["product_name_lenght","product_description_lenght","product_weight_g","product_length_cm","product_height_cm","product_width_cm"],axis=1,inplace=True)
 
 df9["product_photos_qty"].fillna(0.0,inplace=True)
+df9["product_photos_qty"] = df9["product_photos_qty"] .astype("int")
+df9["product_category_name"]=df9["product_category_name"].str.replace("_"," ")
 
 engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-cf3enqun6mplnpe950v0-a.oregon-postgres.render.com:5432/olist')
 
 df9.to_sql('products', engine, if_exists='append', index=False)
+
+print('df9 cargada')
 
 
 df5=pd.read_csv("Datasets/olist_order_items_dataset.csv")
@@ -155,5 +183,18 @@ engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-
 
 df5.to_sql('order_items', engine, if_exists='append', index=False)
 
+print('df5 cargada')
 
 engine.dispose()
+
+# Obtiene el tiempo final
+end_time = time.time()
+
+# Calcula el tiempo de ejecución total
+total_time = end_time - start_time
+
+# Convierte el tiempo de ejecución total a minutos y segundos
+minutes, seconds = divmod(total_time, 60)
+
+# Imprime el tiempo de ejecución total en minutos y segundos
+print("Tiempo de ejecución total: {:.0f} minutos y {:.2f} segundos".format(minutes, seconds))
