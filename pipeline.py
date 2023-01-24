7840db802e4feb2c3b0cd901a96a32d7986534b3
Jucepos <jpostigo89@gmail.com>
2023-01-24 15:10:48 -0300

    agregamos columna

--- pipeline.py
+++ pipeline.py
@@ -161,6 +161,8 @@ def etl_customers():
 # Orders
 
 def etl_orders(orders):
+    payments=pd.read_csv("Datasets/olist_order_payments_dataset.csv")
+    
     orders["order_purchase_timestamp"]=pd.to_datetime(orders["order_purchase_timestamp"],format="%Y-%m-%d %H:%M:%S")
     orders["order_approved_at"]=pd.to_datetime(orders["order_approved_at"],format="%Y-%m-%d %H:%M:%S")
     orders["order_delivered_carrier_date"]=pd.to_datetime(orders["order_delivered_carrier_date"],format="%Y-%m-%d %H:%M:%S")
@@ -196,6 +198,17 @@ def etl_orders(orders):
     orders.loc[(orders["order_status"]=="delivered")&(orders["order_delivered_customer_date"].isnull()),"order_status"]="shipped"
 
     orders.drop(columns=["difference_days1","difference_days2","difference_days3","difference_days4","difference_days5","order_approved_at_new","order_delivered_carrier_date_new"],inplace=True)
+
+    dftotalordercost=payments.groupby(['order_id']).sum()
+    print(dftotalordercost)
+    dftotalordercost=dftotalordercost.drop(columns=["payment_installments"])
+    print(dftotalordercost)
+    dftotalordercost.rename(columns={"payment_value":"total_order_cost"},inplace=True)
+    print(dftotalordercost)
+   
+    orders = orders.merge(dftotalordercost[['total_order_cost']], on='order_id')
+    #orders=pd.merge(left=orders,right=dftotalordercost,on="order_id",how="outer")
+    
 
     engine = create_engine('postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-cf3enqun6mplnpe950v0-a.oregon-postgres.render.com:5432/olist')
 
