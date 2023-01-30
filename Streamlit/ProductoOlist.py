dced19c0ba591efd611a223741127e1c59104df2
emm-ac <emmanuel.ac6@gmail.com>
2023-01-30 10:17:40 -0300

    Actualización

--- Streamlit/ProductoOlist.py
+++ Streamlit/ProductoOlist.py
@@ -1,18 +1,35 @@
 import streamlit as st
 
-st.set_page_config(page_title='Inicio', 
-                   page_icon='🚀', 
+st.set_page_config(page_title='Olist', 
+                   page_icon=None, 
                    layout="centered", 
-                   initial_sidebar_state="collapsed", 
+                   initial_sidebar_state="auto", 
                    menu_items=None)
 
 
-#body{
-#    font-family: 'Poppins', sans-serif;
-#    background-color: #b9b9b9;
-#    background-image: radial-gradient( ellipse farthest-corner at 40px 40px , #3f8c5e, #3f8c5e 50%, #b9b9b9 50%);
-#    background-size:  15px 30px;
-#}
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.png')
 
 
 st.title('TA Tools :rocket:')
