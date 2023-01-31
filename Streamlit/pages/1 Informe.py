8142afc2155dd4bf7c9325a8d787adbd279fd584
emm-ac <emmanuel.ac6@gmail.com>
2023-01-30 22:58:09 -0300

    Cambios en archivos

--- Streamlit/pages/1 Informe.py
+++ Streamlit/pages/1 Informe.py
@@ -7,41 +7,20 @@ st.set_page_config(page_title='TA Tools - Edades',
                    menu_items=None)
 
 
-# CSS to inject contained in a string
-hide_table_row_index = """
-            <style>
-            thead tr th:first-child {display:none}
-            tbody th {display:none}
-            </style>
-            """
-# Inject CSS with Markdown
-st.markdown(hide_table_row_index, unsafe_allow_html=True)
-# Display a static table
-
-
-header_style = '''
-    <style>
-        th{
-            background-color: yellow;
-        }
-    </style>
-'''
-st.markdown(header_style, unsafe_allow_html=True)
-
-
-st.header('Indicadores de tu grupo')
+st.header('Informe del Análisis de datos')
    
+
+def show_pdf(file_path):
+    with open(file_path,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+show_pdf('post1-compressed.pdf')
+    
+
 
 st.markdown("""
 <embed src="https://drive.google.com/viewerng/
-viewer?embedded=true&url=https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" width="600" height="480">
+viewer?embedded=true&url=https://drive.google.com/file/d/1ZTXhWUK8tRgwE79U8jXtSzphUJDCmHdP/view?usp=sharing" width="800" height="600">
 """, unsafe_allow_html=True)
-
-
-with open("post1-compressed.pdf", "rb") as pdf_file:
-    PDFbyte = pdf_file.read()
-
-st.download_button(label="Descarga PDF", 
-        data=PDFbyte,
-        file_name="pandas-clean-id-column.pdf",
-        mime='application/octet-stream')
