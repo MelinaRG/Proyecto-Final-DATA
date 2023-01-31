6919cec43749730ea3f677deac97d4710a71fa3e
nicolazarte2894 <nicolazarte2894@gmail.com>
2023-01-30 23:04:16 -0300

    Fixing Database connection

--- Streamlit/pages/1_Serie.py
+++ Streamlit/pages/1_Serie.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
-import sqlite3 as sql
 import plotly.graph_objects as go
 from prophet import Prophet
 from prophet.diagnostics import cross_validation
@@ -11,30 +10,29 @@ from prophet.diagnostics import performance_metrics
 from prophet.plot import plot_cross_validation_metric
 from prophet.serialize import model_to_json, model_from_json
 
-
 st.title('Serie de tiempo - Ventas diarias :chart_with_upwards_trend:')
-
 st.title('Forecasting :chart:')
 
-# Initialize connection.
-# Uses st.experimental_singleton to only run once.
+# DATABASE #
 
 
-@st.experimental_singleton(show_spinner=True)
+@st.experimental_singleton
 def init_connection():
-    # Engine con las credenciales
-    engine = create_engine(
-        'postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-cf3enqun6mplnpe950v0-a.oregon-postgres.render.com:5432/olist')
-    connection = engine.connect()
-    return connection
+    return psycopg2.connect("host='datapfpostgres.postgres.database.azure.com' port='5432' dbname='postgres' user='meli@datapfpostgres' password='hola123#'")
+# 'postgresql://olist:IHCRtcefMFbJIjUMXuUMtcIfpTAEo5d1@dpg-cf3enqun6mplnpe950v0-a.oregon-postgres.render.com:5432/olist'
 
 
 @st.experimental_memo(ttl=600)
 def get_df_transformed():
-    orders = pd.read_sql_table('orders', conn)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM orders")
+    results = cursor.fetchall()
+    column_names = [column[0] for column in cursor.description]
+    orders = pd.DataFrame(results, columns=column_names)
     orders["order_purchase_timestamp"] = pd.to_datetime(
         orders["order_purchase_timestamp"], format="%Y-%m-%d %H:%M:%S")
     orders['total_order_cost'] = orders['total_order_cost'].astype('float')
+
     # Trasformaciones
     orders.dropna(subset=['total_order_cost'], inplace=True)
     df_ts = orders[orders['order_status'].isin(
@@ -50,15 +48,17 @@ def get_df_transformed():
     df_d = df_d.loc['2017-01-01':'2018-08-31']
 
     # Cierre de conexiones
-    # cursor.close()
+    cursor.close()
     return df_d
 
 
 conn = init_connection()
 df_d = get_df_transformed()
-
 conn.close()
 
-st.header('Serie de tiempo - Datos reales')
-#st.area_chart(df_d, x=df_d.index, y=df_d['y'])
 st.table(df_d)
+
+# CARGA DEL MODELO #
+
+with open('serialized_model.json', 'r') as fin:
+    m = model_from_json(fin.read())
